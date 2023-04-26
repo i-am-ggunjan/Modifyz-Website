d6e8c670552003b1daeaf3d984adf753f3b7f44e
i-am-ggunjan <gauravgunjan200301@gmail.com>
2023-04-26 11:25:08 +0530

    Update form for validation feat: ReactHook Form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,18 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { Store } from "../api/RegStore";
 import "../styles/Form.css";
+import { useForm } from "react-hook-form";
 
 const Form = () => {
-  let [num, setNum] = useState();
   let data = useContext(Store);
+
+  const form = useForm();
+  const { register, handleSubmit, formState } = form;
+  const { errors } = formState;
+
+  const onSubmit = (value) => {
+    data.handleCreate(value.number);
+  };
 
   return (
     <>
@@ -14,18 +22,29 @@ const Form = () => {
           <p>Asap</p>
         </div>
 
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            data.handleCreate(num);
-          }}
-        >
-          <input
-            onChange={(e) => setNum(e.target.value)}
-            className="shadow-xl"
-            type="number"
-            placeholder="Enter you number"
-          />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <span>
+            <input
+              type="number"
+              className="shadow-xl"
+              placeholder="Enter you Number"
+              {...register("number", {
+                required: {
+                  value: true,
+                  message: "Number is required",
+                },
+                maxLength: {
+                  value: 10,
+                  message: "Limit the max-length",
+                },
+                minLength: {
+                  value: 10,
+                  message: "Less than required value ",
+                },
+              })}
+            />
+            <p className="text-red-600 text-xl">{errors.number?.message}</p>
+          </span>
           <button>Register</button>
         </form>
       </section>
